73cede3ec48e9dc4ea398ff46bbf4f07201a94ca
Bart Lanen <b.bart.lanen@gmail.com>
2017-11-30 11:34:18 +0100

    init

---
+++ pepper_arcs/__init__.py
@@ -0,0 +1,1 @@
+from arcs import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pepper_arcs',
+      version='0.1',
+      author='Laura de Koning',
+      packages=['pepper_arcs'],
+      install_requires=['numpy', 'pandas'],
+      zip_safe=False
+)
